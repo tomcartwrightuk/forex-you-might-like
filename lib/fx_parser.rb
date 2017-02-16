d851658cc9f989aadb38f4d72846f65bd5565b41
Tom Cartwright <tecartwright@gmail.com>
2017-02-16 09:42:41 +0000

    Initial feed parser implmentation and test

---
+++ lib/fx_parser.rb
@@ -0,0 +1,8 @@
+# this class is slightly unnessary for this particular implementation but it's how I would likely build the system if it was going to deal with multiple feed sources
+
+class FxParser
+  def parse
+    raise NotImplementedError,
+      "Implement #{__method__} in subclass"
+  end
+end
---
+++ spec/lib/ecb_parser_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe EcbParser do
+  describe "#parse" do
+    context "parsable file" do
+      it "should return an array of exchange rates" do
+        xml_doc = File.open("spec/fixtures/ecb-res.xml")
+        res = EcbParser.parse(xml_doc)
+        expect(res).to include({
+          date: Date.new(2017,2,15),
+          currency: "USD",
+          rate: BigDecimal("1.0555")
+        })
+        expect(res.length).to eq 4
+      end
+    end
+
+    context "unparseable file" do
+      it "should raise error for unparsable date"
+      it "should raise error for unparsable rate"
+      it "should raise missing data error for"
+    end
+  end
+end
